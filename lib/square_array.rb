ffd157e6f07b0ac88597ed7349e9fb4f528c9d85
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-30 14:34:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,9 +9,9 @@ def square_array(numbers)
   counter = 0
   new_array = []
   while numbers[counter] do
-    counter += 1
     sqrd_nums = numbers[counter]**2
     new_array << sqrd_nums
-    return new_array
+    counter += 1
   end
+  return new_array
 end
